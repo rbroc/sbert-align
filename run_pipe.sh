c68fd871c0cbdeb39c7f4d412c0291769bad1c1e
rbroc <rbrrcc@gmail.com>
2022-12-09 10:43:28 -0600

    add sampled ids to data

--- run_pipe.sh
+++ run_pipe.sh
@@ -7,6 +7,6 @@
 python3 align.py --lag 2 --pair-type true --model all-MiniLM-L6-v2
 python3 align.py --lag 2 --pair-type surrogate --model all-MiniLM-L6-v2
 
-git add outputs/*
-git commit -am 'add output files'
-git push 
+#git add outputs/*
+#git commit -am 'add output files'
+#git push 
