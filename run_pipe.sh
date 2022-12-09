6acfbd8cfd345f65c57535253d182bd9b64b5282
rbroc <rbrrcc@gmail.com>
2022-12-09 18:07:36 +0100

    adapt script

--- run_pipe.sh
+++ run_pipe.sh
@@ -2,9 +2,9 @@
 
 #python3 ./scripts/get_surrogate_ids.py --sampled-ids 50
 
-for lag in 3
+for lag in 1 2 3
 do
-    for model in all-MiniLM-L6-v2 #all-mpnet-base-v2
+    for model in all-mpnet-base-v2 # all-MiniLM-L6-v2 
     do
         for ptype in surrogate true
         do
